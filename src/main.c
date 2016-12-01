977afa62740831a3ef67031148fc2399e20050fc
ryanisaacg <ryan@ryanisaacg.com>
2016-12-01 15:27:35 -0500

    Added more configuration and a method to spawn things
    The build doesn't work though

--- src/main.c
+++ src/main.c
@@ -6,10 +6,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+SDL_Texture *player_texture, *hook_texture;
+float player_jump, player_walk, player_gravity, player_gravity_hold, player_width, player_height, player_drag_x, player_max_x, player_max_y;
+float hook_width, hook_height, hook_speed;
+SDL_Renderer *rend;
+
+typedef enum EntityType {ENTITY_PLAYER, ENTITY_HOOK} EntityType;
+
 typedef struct {
 	SDL_Texture *texture;
-	enum {ENTITY_PLAYER} type;
+	enum EntityType type;
+	union {
+		struct { ArcadeObject *hook; } player;
+		struct { ArcadeObject *parent, *target; } hook;
+	} specific;
 } EntityData;
+
+ArcadeObject *new_entity(World *world, Vector2 position, EntityType type) {
+	EntityData *data = malloc(sizeof(*data));
+	data->type = type;
+	Shape bounds;
+	Vector2 acceleration = vec2_new(0, 0), drag = vec2_new(0, 0), max_velocity = vec2_new(0, 0);
+	bool solid = false;
+	switch(type) {
+		case ENTITY_PLAYER:
+			bounds = shape_rect(rect_new(position.x, position.y, player_width, player_height));
+			acceleration.y = player_gravity;
+			drag.x = player_drag_x;
+			max_velocity = vec2_new(player_max_x, player_max_y);
+			data->texture = player_texture;
+			break;
+		case ENTITY_HOOK:
+			bounds = shape_rect(rect_new(position.x, position.y, hook_width, hook_height));
+			data->texture = hook_texture;
+			data->specific.hook.target = NULL;
+			data->specific.hook.parent = NULL;
+			break;
+	}
+	ArcadeObject obj = arcobj_new(bounds, solid, data);
+	obj.acceleration = acceleration;
+	obj.drag = drag;
+	obj.max_velocity = max_velocity;
+	size_t index = world_add(world, obj);
+	ArcadeObject *current = world_get(*world, index);
+	if(type == ENTITY_PLAYER) {
+		ArcadeObject *hook = new_entity(world, position, ENTITY_HOOK);
+		hook->alive = false;
+		EntityData *hook_data = hook->data;
+		data->specific.hook.parent = current;
+		EntityData *player_data = current->data;
+		player_data->specific.player.hook = hook;
+	}
+	return current;
+}
 
 SDL_Texture* load_texture(SDL_Renderer *rend, char *path) {
 	//The final texture
@@ -30,8 +79,6 @@ SDL_Texture* load_texture(SDL_Renderer *rend, char *path) {
 	}
 	return newTexture;
 }
-
-float player_jump, player_walk, player, player_gravity, player_gravity_hold;
 
 void update(World world, ArcadeObject *obj) {
 	EntityData *data = obj->data;
@@ -61,6 +108,18 @@ void update(World world, ArcadeObject *obj) {
 	}
 }
 
+void draw(ArcadeObject *obj) {
+	Rect bounds = shape_bounding_box(obj->bounds);
+	EntityData *data = obj->data;
+	SDL_Rect rect = {bounds.x, bounds.y, bounds.width, bounds.height};
+	SDL_RenderCopy(rend, data->texture, NULL, &rect);
+}
+
+void frame(World world, ArcadeObject *obj) {
+	update(world, obj);
+	draw(obj);
+}
+
 void collide(ArcadeObject *a, ArcadeObject *b) {
 
 }
@@ -85,34 +144,36 @@ int main() {
 		exit(-1);
 	}
 	//Create renderer for window
-	SDL_Renderer *rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (rend == NULL) {
 		printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
 		exit(-1);
 	}
 	//Load the player texture
-	SDL_Texture *texture = load_texture(rend, "../img/player.png");
+	player_texture 	= load_texture(rend, "../img/player.png");
+	hook_texture 	= load_texture(rend, "../img/player.png");
 	//Load the config file
-	JSON_Object *config = json_value_get_object(json_parse_file("../data/config.json"));
-	JSON_Object *player_config = json_object_get_object(config, "player");
-	float gravity = json_object_get_number(config, "gravity");
-	
+	JSON_Object *config 		= json_value_get_object(json_parse_file("../data/config.json"));
+	JSON_Object *player_config 	= json_object_get_object(config, "player");
+	JSON_Object *hook_config 	= json_object_get_object(config, "hook");
+	//Global values
+	player_walk 		= json_object_get_number(player_config, "walk"); 
+	player_jump 		= json_object_get_number(player_config, "jump");
+	player_gravity 		= json_object_get_number(player_config, "gravity"); 
+	player_gravity_hold = json_object_get_number(player_config, "hold-gravity");
+	player_width 		= json_object_get_number(player_config, "width");
+	player_height 		= json_object_get_number(player_config, "height");
+	player_drag_x 		= json_object_get_number(player_config, "drag-x");
+	player_max_x 		= json_object_get_number(player_config, "max-x");
+	player_max_y 		= json_object_get_number(player_config, "max-y");
+	hook_width			= json_object_get_number(hook_config, "width");
+	hook_height			= json_object_get_number(hook_config, "height");
+	hook_speed			= json_object_get_number(hook_config, "speed");
 	//Create the simulation world
 	World world = world_new(640, 480, 96);
 	TileMap map = tl_new(sizeof(SDL_Texture*), 640, 480, 32);
 	world_add_tilemap(&world, map);
-	EntityData *data = malloc(sizeof(data));
-	*data = (EntityData){ texture, ENTITY_PLAYER };
-	ArcadeObject obj = arcobj_new(shape_rect(rect_new(0, 0, 32, 32)), false, data);
-	obj.acceleration.y = json_object_get_number(player_config, "gravity");
-	obj.max_velocity = vec2_new(json_object_get_number(player_config, "max_x"), json_object_get_number(player_config, "max_y"));
-	obj.drag.x = json_object_get_number(player_config, "drag_x");
-	//Global values
-	player_walk = json_object_get_number(player_config, "walk"); 
-	player_jump = json_object_get_number(player_config, "jump");
-	player_gravity = json_object_get_number(player_config, "gravity"); 
-	player_gravity_hold = json_object_get_number(player_config, "hold-gravity");
-	world_add(&world, obj);
+	new_entity(&world, vec2_new(0, 0), ENTITY_PLAYER);
 	while(true) {
 		SDL_Event event;
 		while(SDL_PollEvent(&event)) {
@@ -121,16 +182,8 @@ int main() {
 					goto cleanup;
 			}
 		}
-		ArcadeObject *player = qt_get(world.entities, 0);
-		world_update(world, 1, &update, &collide);
 		SDL_RenderClear(rend);
-		for(size_t i = 0; i < qt_len(world.entities); i++) {
-			ArcadeObject *obj = world_get(world, i);
-			Rect bounds = shape_bounding_box(obj->bounds);
-			EntityData *data = obj->data;
-			SDL_Rect rect = {bounds.x, bounds.y, bounds.width, bounds.height};
-			SDL_RenderCopy(rend, data->texture, NULL, &rect);
-		}
+		world_update(world, 1, &frame, &collide);
 		SDL_RenderPresent(rend);
 		SDL_Delay(10);
 	}
